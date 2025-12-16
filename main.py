a296064bfc3de9fcd48780f71b3b8d79c01d4f69
LordDeatHunter <desuhansa@gmail.com>
2025-12-16 22:18:51 +0100

    Add endpoint for listing all techs

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
+
+
+def load_techs():
+    techs_dir = "assets/tech"
+
+    def remove_ext(filename):
+        return filename.split('.')[0].split('_')[0]
+
+    def get_files(dir_path):
+        return [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+
+    return list(set([remove_ext(f) for f in get_files(techs_dir)]))
+
+
+techs = load_techs()
+
+
+@app.get("/techs")
+def get_techs():
+    return {"techs": techs}
 
 
 @app.get("/badge")
