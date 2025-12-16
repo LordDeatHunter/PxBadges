7289381bcbef07aba9e1a6419bea90ed145b57fa
LordDeatHunter <desuhansa@gmail.com>
2025-12-17 00:10:50 +0100

    Generate images properly

--- image_generator.py
+++ image_generator.py
@@ -1,17 +1,28 @@
 from PIL import Image
 from io import BytesIO
 
+STAR_LOCATIONS = [(7, 46), (19, 47), (31, 46)]
+LOGO_LOCATION = (8, 10)
 
 def generate_badge(tech: str, score: int, scale: int) -> BytesIO:
-    base = Image.open("assets/badge_1.png").convert("RGBA")
+    badge = Image.open("assets/badge_1.png").convert("RGBA")
+    logo = Image.open(f"assets/tech/{tech}.png").convert("RGBA")
     star = Image.open("assets/star.png").convert("RGBA")
+    half_star = star.crop((0, 0, star.width // 2, star.height)).convert("RGBA")
 
-    base.paste(star, (7, 46), star)
-    base.paste(star, (19, 48), star)
-    base.paste(star, (31, 46), star)
+    for i in range(len(STAR_LOCATIONS)):
+        current_pos = i * 2 + 1
+        if current_pos < score:
+            badge.paste(star, STAR_LOCATIONS[i], star)
+        elif current_pos == score:
+            badge.paste(half_star, STAR_LOCATIONS[i], half_star)
+
+    badge.paste(logo, LOGO_LOCATION, logo)
+
+    badge = badge.resize((badge.width * scale, badge.height * scale), Image.Resampling.NEAREST)
 
     output = BytesIO()
-    base.save(output, format="PNG")
+    badge.save(output, format="PNG")
     output.seek(0)
 
     return output
